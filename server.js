1d65364246e2666978e03b332047b6b6d10ea314
ink <eric.zmo2@gmail.com>
2020-05-23 15:56:15 -0700

    mongoose fix

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 // routes
